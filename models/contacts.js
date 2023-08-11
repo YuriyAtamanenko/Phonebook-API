eba2bb418b0f5077464af67b769fa3b0716acff1
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-08-11 17:55:04 +0300

    upd hw03

--- models/contacts.js
+++ models/contacts.js
@@ -1,21 +1,26 @@
 const { Schema, model } = require("mongoose");
 
-const contactSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, "Set name for contact"],
+const contactSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-  },
-  phone: {
-    type: String,
-  },
-  favorite: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    versionKey: false,
+  }
+);
 
 const Contact = model("contacts", contactSchema);
 
