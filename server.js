88a28cb6802b8099445df0421e2106bfe0958cc5
YuriyAtamanenko <118563472+YuriyAtamanenko@users.noreply.github.com>
2023-10-14 16:57:09 +0300

    add node version

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { DB_HOST, PORT = 3000 } = process.env;
 mongoose.set("strictQuery", true);
 
 mongoose
-  .connect(DB_HOST, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(DB_HOST)
   .then(() =>
     app.listen(PORT, () => console.log("Database connection successful"))
   )
